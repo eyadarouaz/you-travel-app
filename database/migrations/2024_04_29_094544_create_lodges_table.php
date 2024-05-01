fcf3218c61aee87bb9c432586383f17e2e9c4d37
eyadarouaz <eyaadarouez@gmail.com>
2024-05-01 11:29:53 +0100

    feat: add lodges table migration

---
+++ database/migrations/2024_04_29_094544_create_lodges_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lodges', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->date('check_in');
+            $table->date('check_out');
+            $table->unsignedBigInteger('trip_id');
+            $table->timestamps();
+            $table->foreign('trip_id')
+                  ->references('id')->on('trips')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lodges');
+    }
+};
